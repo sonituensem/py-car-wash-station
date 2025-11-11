f5f6f8708284ce24d8b7a41f1a508ec50f38e0c8
Yatsenko Dmytro <sonituensem@gmail.com>
2025-11-11 11:32:21 +0200

    Fixed flake8 issues

--- app/main.py
+++ app/main.py
@@ -2,26 +2,54 @@ from typing import List
 
 
 class Car:
+<<<<<<< HEAD
     def __init__(self, comfort_class: int,
                  clean_mark: int, brand: str) -> None:
+=======
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
+>>>>>>> 7816b8b (Fixed flake8 issues)
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
+<<<<<<< HEAD
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
+=======
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
+>>>>>>> 7816b8b (Fixed flake8 issues)
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
+<<<<<<< HEAD
         price = (car.comfort_class
                  * (self.clean_power - car.clean_mark)
                  * self.average_rating
                  / self.distance_from_city_center)
+=======
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+>>>>>>> 7816b8b (Fixed flake8 issues)
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -37,7 +65,11 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
+<<<<<<< HEAD
     def rate_service(self, new_rating: int) -> None:
+=======
+    def rate_service(self, new_rating: float) -> None:
+>>>>>>> 7816b8b (Fixed flake8 issues)
         total_rating = self.average_rating * self.count_of_ratings
         total_rating += new_rating
         self.count_of_ratings += 1
